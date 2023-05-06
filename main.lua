0b38c4c4f1fbdc721a1398b18985de2bcc2a418e
ciara1barry <ciara2barry@gmail.com>
2023-05-06 12:56:55 +0100

    Finished functionality for 'Power Morpher' item

--- main.lua
+++ main.lua
@@ -1,26 +1,41 @@
 local mod = RegisterMod("Power Coin", 1)
 local powerCoin = Isaac.GetItemIdByName("Power Coin")
 local powerCoinDamage = 1
+local powerMorpher = Isaac.GetItemIdByName("Power Morpher")
+local useCount = 0
 
 function mod:EvaluateCache(player, cacheFlags)
     if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
         local itemCount = player:GetCollectibleNum(powerCoin)
         local damageToAdd = powerCoinDamage * itemCount
         player.Damage = player.Damage + damageToAdd
+        useCount = 0
     end
 end
-
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
-
-local powerMorpher = Isaac.GetItemIdByName("Power Morpher")
 
 function mod:PowerMorpherUse(item)
     local player = Isaac.GetPlayer(0)
     local itemCount = player:GetCollectibleNum(powerCoin)
-    if itemCount > 0 then
-        player.Damage = (player.Damage * 1.5) + 1.5 else
-            player.Damage = player.Damage + 1.5
+
+    if useCount > 0 then
+        if itemCount > 0 then
+            player.Damage = (player.Damage - 1.5)/1.5 else
+                player.Damage = player.Damage - 1.5
+        end 
+        useCount = 0 else
+            if itemCount > 0 then
+                player:AddSoulHearts(2)
+            end
     end
+
+    if itemCount > 0 then
+        player.Damage = (player.Damage * 1.5) + 1.5
+        useCount = useCount + 1 else
+            player.Damage = player.Damage + 1.5
+            useCount = useCount + 1
+    end
+
+    Isaac.ConsoleOutput(tostring(useCount))
 
     return {
         Discharge = true,
@@ -29,4 +44,5 @@ function mod:PowerMorpherUse(item)
     }
 end
 
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.PowerMorpherUse, powerMorpher)
