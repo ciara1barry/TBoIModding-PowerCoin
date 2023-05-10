2dd4d2e43f7eb0814fc5c9c7f83641f75090d44f
ciara1barry <ciara2barry@gmail.com>
2023-05-10 13:53:18 +0100

    Added 'Broken Power Morpher' item

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local mod = RegisterMod("Power Coin", 1)
 local powerCoin = Isaac.GetItemIdByName("Power Coin")
 local powerCoinDamage = 1
 local powerMorpher = Isaac.GetItemIdByName("Power Morpher")
+local brokenPowerMorpher = Isaac.GetItemIdByName("Broken Power Morpher")
 local useCount = 0
 
 function mod:EnterNewRoom()
@@ -53,6 +54,32 @@ function mod:PowerMorpherUse(item)
     }
 end
 
+function mod:BrokenPowerMorpherUse(item)
+    local player = Isaac.GetPlayer(0)
+    local itemCount = player:GetCollectibleNum(powerCoin)
+    
+    if itemCount > 0 then
+        local brokenHeartChance = math.random(1, 10)
+        if brokenHeartChance <= 5 then
+            player:AddBrokenHearts(1)
+        end
+    end
+    
+    if itemCount > 0 and useCount == 0 then
+        player.Damage = (player.Damage * 1.5) + 1.5
+        useCount = useCount + 1 elseif itemCount == 0 and useCount == 0 then
+            player.Damage = player.Damage + 1.5
+            useCount = useCount + 1
+    end
+
+    return {
+        Discharge = true,
+        Remove = false,
+        ShowAnim = true
+    }
+end
+
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.EnterNewRoom)
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.PowerMorpherUse, powerMorpher)
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.BrokenPowerMorpherUse, brokenPowerMorpher)
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
