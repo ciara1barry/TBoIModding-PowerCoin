bb1e0bd5a9f876a2cb919819849db7ac64304f01
ciara1barry <ciara2barry@gmail.com>
2023-05-12 00:20:22 +0100

    Added and implemented all remaining 'Dino Gems'

--- main.lua
+++ main.lua
@@ -11,10 +11,18 @@ local whiteDinoGem = Isaac.GetTrinketIdByName("White Dino Gem")
 local SLOW_COLOUR = Color(100, 100, 100)
 local blackDinoGem = Isaac.GetTrinketIdByName("Black Dino Gem")
 local CAMO_COLOUR = Color(1, 1, 1, 0.3, 1, 1, 1)
+local redDinoGem = Isaac.GetTrinketIdByName("Red Dino Gem")
+local DINO_GEM_CHANCE = 0.10
+local RED_DINO_GEM_MOVE_SPEED = false
+local yellowDinoGem = Isaac.GetTrinketIdByName("Yellow Dino Gem")
+local YELLOW_DINO_GEM_REVELATION = false
+local blueDinoGem = Isaac.GetTrinketIdByName("Blue Dino Gem")
+local BLUE_DINO_GEM_WAFER = false
 
 function mod:EnterNewRoom()
     local player = Isaac.GetPlayer(0)
     local entities = Isaac.GetRoomEntities()
+    local rng = player.GetTrinketRNG(player, redDinoGem)
     
     local itemCount = player:GetCollectibleNum(powerCoin)
     if useCount > 0 then
@@ -34,12 +42,45 @@ function mod:EnterNewRoom()
     end
 
     if (player:GetTrinket(0) or player:GetTrinket(1)) == blackDinoGem then
-        for i, entity in ipairs(entities) do
+        for _, entity in ipairs(entities) do
             if entity:IsActiveEnemy() then
                 entity:AddConfusion(EntityRef(player), 150, false)
             end
         end
         player:SetColor(CAMO_COLOUR, 150, 1, false, false)
+    end
+
+    if (player:GetTrinket(0) or player:GetTrinket(1)) == redDinoGem then
+        if rng:RandomFloat() < DINO_GEM_CHANCE then
+            player.MoveSpeed = player.MoveSpeed + 1
+            RED_DINO_GEM_MOVE_SPEED = true else
+                if RED_DINO_GEM_MOVE_SPEED == true then
+                    player.MoveSpeed = player.MoveSpeed - 1
+                    RED_DINO_GEM_MOVE_SPEED = false
+                end
+        end
+    end
+
+    if (player:GetTrinket(0) or player:GetTrinket(1)) == blueDinoGem then
+        if rng:RandomFloat() < DINO_GEM_CHANCE then
+            player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_WAFER)
+            BLUE_DINO_GEM_WAFER = true else
+                if BLUE_DINO_GEM_WAFER == true then
+                    player:ClearTemporaryEffects()
+                    BLUE_DINO_GEM_WAFER = false
+                end
+        end
+    end
+
+    if (player:GetTrinket(0) or player:GetTrinket(1)) == yellowDinoGem then
+        if rng:RandomFloat() < DINO_GEM_CHANCE then
+            player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_REVELATION)
+            YELLOW_DINO_GEM_REVELATION = true else
+                if YELLOW_DINO_GEM_REVELATION == true then
+                    player:ClearTemporaryEffects()
+                    YELLOW_DINO_GEM_REVELATION = false
+                end
+        end
     end
 end
 
